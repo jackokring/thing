6c8f5a08df1a3bbe5eb5c16ea0b79d636c193f7a
jackokring <jackokring@gmail.com>
2025-10-30 08:45:21 +0000

    Default Fabric Project

---
+++ src/client/java/uk/co/kring/thing/ThingClient.java
@@ -0,0 +1,10 @@
+package uk.co.kring.thing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/uk/co/kring/thing/ThingDataGenerator.java
@@ -0,0 +1,11 @@
+package uk.co.kring.thing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
