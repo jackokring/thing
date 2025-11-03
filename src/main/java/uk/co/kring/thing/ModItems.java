87d2c3ea3910ba901960d0c38dd45398f40449cc
jackokring <jackokring@gmail.com>
2025-11-03 22:36:01 +0000

    Just say no to custom item groups

--- src/main/java/uk/co/kring/thing/ModItems.java
+++ src/main/java/uk/co/kring/thing/ModItems.java
@@ -1,16 +1,12 @@
 package uk.co.kring.thing;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.text.Text;
 
 import java.util.function.Function;
 
