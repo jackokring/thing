d84581351d6fad12da1434fb78a106e97e9389a1
jackokring <jackokring@gmail.com>
2025-11-04 11:55:02 +0000

    Reduce, reuse, recycle

--- src/main/java/uk/co/kring/thing/ModItems.java
+++ src/main/java/uk/co/kring/thing/ModItems.java
@@ -1,6 +1,8 @@
 package uk.co.kring.thing;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -11,7 +13,9 @@ import net.minecraft.registry.RegistryKeys;
 import java.util.function.Function;
 
 public class ModItems {
-    public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+    // all items in the reduce, reuse, recycle mindset
+    public static final Item SUSPICIOUS_SUBSTANCE = register(
+            "suspicious_substance", Item::new, new Item.Settings(), 0.1f, 0);
 
     public static void initialize() {
         // Just say no to custom item groups as the botchy big G says
@@ -21,7 +25,8 @@ public class ModItems {
         });
     }
 
-    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings,
+                                float compostChance, int fuelSeconds) {
         // Create the item key.
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Thing.identify(name));
 
@@ -30,6 +35,17 @@ public class ModItems {
 
         // Register the item.
         Registry.register(Registries.ITEM, itemKey, item);
+
+        if(compostChance > 0.0f)
+            // Add the suspicious substance to the composting registry with a 30% chance of increasing the composter's level.
+            CompostingChanceRegistry.INSTANCE.add(ModItems.SUSPICIOUS_SUBSTANCE, compostChance);
+
+        // Add the suspicious substance to the registry of fuels, with a burn time of 30 seconds.
+        // Remember, Minecraft deals with logical based-time using ticks.
+        // 20 ticks = 1 second.
+        if(fuelSeconds > 0)
+            FuelRegistryEvents.BUILD.register((builder, context) ->
+                    builder.add(ModItems.SUSPICIOUS_SUBSTANCE, fuelSeconds * 20));
 
         return item;
     }
