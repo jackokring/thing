d87ce022dcc61690a077f45a5d0d2d0bbc10c5f2
jackokring <jackokring@gmail.com>
2025-11-04 12:28:31 +0000

    Always edible, no reason to eat, regret

--- src/main/java/uk/co/kring/thing/ModItems.java
+++ src/main/java/uk/co/kring/thing/ModItems.java
@@ -3,9 +3,14 @@ package uk.co.kring.thing;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.ConsumableComponents;
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.consume.ApplyEffectsConsumeEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -16,10 +21,16 @@ import java.util.function.Function;
 public class ModItems {
     // hey fatty
     public static final FoodComponent EDIBLE = new FoodComponent.Builder().alwaysEdible().build();
+    public static final ConsumableComponent OH_MY_TUMMY = ConsumableComponents.food()
+            // The duration is in ticks, 20 ticks = 1 second
+            .consumeEffect(new ApplyEffectsConsumeEffect(new StatusEffectInstance(StatusEffects.POISON,
+                    6 * 20, 1), 1.0f))
+            .build();
 
     // all items in the reduce, reuse, recycle mindset
     public static final Item SUSPICIOUS_SUBSTANCE = register(
-            "suspicious_substance", Item::new, new Item.Settings().food(EDIBLE), 0.1f, 5);
+            "suspicious_substance", Item::new, new Item.Settings().food(EDIBLE, OH_MY_TUMMY),
+            0.1f, 5);
 
     public static void initialize() {
         // Just say no to custom item groups as the botchy big G says
