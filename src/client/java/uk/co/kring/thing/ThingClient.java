c34b04ba2f5b15631ecdc100d17c8eb80f9321c8
jackokring <jackokring@gmail.com>
2025-11-04 19:42:13 +0000

    ItemTooltip CLIENT_SIDE, and idiot var

--- src/client/java/uk/co/kring/thing/ThingClient.java
+++ src/client/java/uk/co/kring/thing/ThingClient.java
@@ -1,10 +1,16 @@
 package uk.co.kring.thing;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.text.Text;
 
 public class ThingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list) -> {
+            if(itemStack.isOf(ModItems.SUSPICIOUS_SUBSTANCE))
+                list.add(Text.translatable(itemStack.getItem().getTranslationKey() + ".tooltip"));
+        });
 	}
 }
