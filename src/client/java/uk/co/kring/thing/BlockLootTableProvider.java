701e7b370f2ae98d5bee0b7467cef69ae056f928
jackokring <jackokring@gmail.com>
2025-11-05 23:33:09 +0000

    Mining 2.0

--- src/client/java/uk/co/kring/thing/BlockLootTableProvider.java
+++ src/client/java/uk/co/kring/thing/BlockLootTableProvider.java
@@ -3,6 +3,8 @@ package uk.co.kring.thing;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.world.level.storage.loot.providers.number.BinomialDistributionGenerator;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -12,11 +14,15 @@ public class BlockLootTableProvider extends FabricBlockLootTableProvider {
         super(dataOutput, registryLookup);
     }
 
+    public float inverse(float f) {
+        return 1/f;
+    }
+
     @Override
     public void generate() {
-        // Make suspicious dirt drop its block item.
-        // Also adds the condition that it survives the explosion that broke it, if applicable,
-        createSingleItemTable(ModBlocks.SUSPICIOUS_DIRT);
+        // Make suspicious dirt drop
+        add(ModBlocks.SUSPICIOUS_DIRT, createSingleItemTable(ModItems.SUSPICIOUS_SUBSTANCE,
+                new BinomialDistributionGenerator(new ConstantValue(18), new ConstantValue(inverse(2f)))));
 
         // Make prismarine lamps drop themselves with silk touch only
         //createSilkTouchOnlyTable(ModBlocks.PRISMARINE_LAMP);
