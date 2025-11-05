5291059e5e184ec8676de701736b66cb0a3cc7cb
jackokring <jackokring@gmail.com>
2025-11-05 16:12:04 +0000

    Some tooling tips

--- src/client/java/uk/co/kring/thing/ThingClient.java
+++ src/client/java/uk/co/kring/thing/ThingClient.java
@@ -2,7 +2,11 @@ package uk.co.kring.thing;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+
+import java.util.List;
 
 public class ThingClient implements ClientModInitializer {
     public static String tooltipKey(ItemLike item) {
@@ -12,9 +16,15 @@ public class ThingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list) -> {
-            if(itemStack.isOf(ModItems.SUSPICIOUS_SUBSTANCE))
-                list.add(Text.translatable(tooltipKey(itemStack.getItem())));
+        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext,
+                                            tooltipType, list) -> {
+                exec(ModItems.SUSPICIOUS_SUBSTANCE, itemStack, list);
         });
 	}
+
+    public static void exec(ItemLike is, ItemStack itemStack, List<Component> list) {
+        if(itemStack.getItem() == is)
+            list.add(Component.translatable(tooltipKey(is)));
+
+    }
 }
