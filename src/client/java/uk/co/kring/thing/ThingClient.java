bc5740f55f97a2b7833e9572ef8f7ba46f9fc0a5
jackokring <jackokring@gmail.com>
2025-11-08 21:18:13 +0000

    WIP

--- src/client/java/uk/co/kring/thing/ThingClient.java
+++ src/client/java/uk/co/kring/thing/ThingClient.java
@@ -1,17 +1,35 @@
 package uk.co.kring.thing;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.minecraft.client.MinecraftClient;
+import com.mojang.brigadier.Message;
 import eu.pb4.placeholders.api.Placeholders;
 import eu.pb4.placeholders.api.parsers.NodeParser;
 import eu.pb4.placeholders.api.parsers.TagParser;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.network.chat.SignedMessage;
 import net.minecraft.world.level.ItemLike;
+import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
 import java.util.HashMap;
 
 public class ThingClient implements ClientModInitializer {
@@ -35,7 +53,16 @@ public class ThingClient implements ClientModInitializer {
             if(tip != null)
                 list.add(tip);
         });
+
+        // Config init
         AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+
+        // Chat interceptor
+        ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
+            // Register event listener for ClientTickEvents.END_CLIENT_TICK
+            ClientReceiveMessageEvents.ALLOW_CHAT.register(this::decryptChatMessage);
+            ClientSendMessageEvents.MODIFY_CHAT.register(this::encryptChatMessage);
+        });
     }
 
     HashMap<ItemLike, MutableComponent> tipMap = new HashMap<>();
@@ -57,5 +84,54 @@ public class ThingClient implements ClientModInitializer {
         // and so the .class file has to be decompiled with parchment too
         // but, yes, that should be it for %placeholder% as default
         return parser.parseNode(in).toText();
+    }
+
+    // The following is a mashed up version of similar to
+    // https://git.brn.systems/BRNSystems/chatencryptor/src/branch/main/src/main/java/systems/brn/chatencryptor/SecureChat.java
+    // Under MIT Licence, but with some adaptations to use cloth config, 1.21.10 an kind of MiniMessage formatting
+
+    boolean decryptChatMessage(Component component, @Nullable PlayerChatMessage playerChatMessage, @Nullable GameProfile gameProfile, ChatType.Bound bound, Instant instant) {
+        TranslatableTextContent content = (TranslatableTextContent) message.getContent();
+        String message_content = content.getArg(1).getString();
+        String player_name = content.getArg(0).getString();
+        if(message_content.startsWith("®") && message_content.endsWith("®")){
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, Config.HANDLER.instance().getRawKey(), Config.HANDLER.instance().getRawIv());
+                String strippedMessage = message_content.substring(1, message_content.length() - 1);
+                byte[] decodedMessage = ChatCoder.decodeFromBmp(strippedMessage);
+                cipher.update(decodedMessage);
+                String decryptedMessage = new String(cipher.doFinal());
+                String outputMessage = "{" + player_name + "} " + decryptedMessage;
+                MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Component.literal(outputMessage));
+                return false;
+            }
+            catch (IllegalBlockSizeException | BadPaddingException | NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException |
+                   InvalidKeyException e){
+                MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.of((Message) e));
+                return true;
+            }
+        }
+        return true;
+    }
+
+    String encryptChatMessage(String message) {
+        if(Config.HANDLER.instance().isEnabled() ^ isShiftPressed()){
+            String encodedMessage;
+            try {
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, Config.HANDLER.instance().getRawKey(), Config.HANDLER.instance().getRawIv());
+                cipher.update(message.getBytes(StandardCharsets.UTF_8));
+                byte[] encryptedMessage = cipher.doFinal();
+                encodedMessage = ChatCoder.encodeToBmp(encryptedMessage);
+            } catch (IllegalBlockSizeException | BadPaddingException | InvalidKeyException | NoSuchPaddingException | InvalidAlgorithmParameterException |
+                     NoSuchAlgorithmException e) {
+                return "";
+            }
+            return '®' + encodedMessage + '®';
+        }
+        else {
+            return message;
+        }
     }
 }
