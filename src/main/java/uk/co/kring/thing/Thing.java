6ee5be304ade18f4dea52dfd89a97f0daaf0b030
jackokring <jackokring@gmail.com>
2025-11-08 15:44:41 +0000

    Package scope

--- src/main/java/uk/co/kring/thing/Thing.java
+++ src/main/java/uk/co/kring/thing/Thing.java
@@ -6,28 +6,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Thing implements ModInitializer {
-	public static final String MOD_ID = "thing";
+	static final String MOD_ID = "thing";
 
-    public static ResourceLocation identify(String name) {
+    static ResourceLocation identify(String name) {
         return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
     }
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Loading");
-        ModBlocks.initialize();
-        // Blocks first to allow Items to exist, so register some things there
-        // for common groups, compost anf fuel
         ModItems.initialize();
+        ModBlocks.initialize();
 
     }
 }
