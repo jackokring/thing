3438c7d9fed0ce6938cdf8cba0142fade33170fc
jackokring <jackokring@gmail.com>
2025-11-08 15:37:36 +0000

    Better placement

--- src/client/java/uk/co/kring/thing/ThingClient.java
+++ src/client/java/uk/co/kring/thing/ThingClient.java
@@ -1,5 +1,8 @@
 package uk.co.kring.thing;
 
+import eu.pb4.placeholders.api.Placeholders;
+import eu.pb4.placeholders.api.parsers.NodeParser;
+import eu.pb4.placeholders.api.parsers.TagParser;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
@@ -39,5 +42,20 @@ public class ThingClient implements ClientModInitializer {
 
     void tipSimple(ItemLike is) {
         tipMap.put(is, Component.translatable(tooltipKey(is)));
+    }
+
+    static Component useSimpleText(String in) {
+        // V1 deprecated
+        //@SuppressWarnings("all")
+        NodeParser parser = NodeParser.merge(TagParser.SIMPLIFIED_TEXT_FORMAT,
+                Placeholders.DEFAULT_PLACEHOLDER_PARSER);
+        // toText accepts PlaceholderContext and ParserContext etc.
+        // So this is apparently how to get a V1 MiniMessage format with placeholder insertions
+        // The documentation appears out of date, and well, who knows?
+
+        // I suppose it's not helped by the source not being compiled against Mojang base
+        // and so the .class file has to be decompiled with parchment too
+        // but, yes, that should be it for %placeholder% as default
+        return parser.parseNode(in).toText();
     }
 }
